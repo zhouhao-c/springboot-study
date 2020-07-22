1f35fa8c43ca68e8acafc00908f9d60c1472d43d
zhouhao_global <15623042186@163.com>
2020-07-22 21:29:01 +0800

    首次提交，spring boot与检索

---
+++ springboot-elastic/src/test/java/com/zh/springbootelastic/SpringbootElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zh.springbootelastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElasticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
